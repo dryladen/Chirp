238db40dffd6589d78852d7f3a5ca27b6bc83514
Laden <delfanladen24@gmail.com>
2023-10-20 23:23:22 +0800

    Create post table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,8 @@ export default function Home() {
 
   const user = useUser();
 
+  const { data } = api.post.getAll.useQuery();
+
   return (
     <>
       <Head>
@@ -18,8 +20,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {!user.isSignedIn && <SignInButton />}
-        {!!user.isSignedIn && <SignOutButton />}
+        <div>
+          {!!user.isSignedIn && <SignOutButton />}
+          {!user.isSignedIn && <SignInButton />}
+        </div>
+        <div>
+          {data?.map((post) => <div key={post.id}>{post.content}</div>)}
+        </div>
       </main>
     </>
   );
